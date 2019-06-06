95366cffc9c4135f67e9eac72f76160a0341dd70
martyna <zarzyckamar@gmail.com>
2019-06-06 10:33:32 +0200

    initial commit

---
+++ src/main/java/com/AutoShop/AutoShopApplication.java
@@ -0,0 +1,13 @@
+package com.AutoShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoShopApplication.class, args);
+	}
+
+}
