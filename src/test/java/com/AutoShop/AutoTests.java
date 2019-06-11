0ef1b7d7977bfcefbed215f2ac5682f948d7f675
izablotny <lUbiegrac02>
2019-06-11 23:36:13 +0200

    change url builder

--- src/test/java/com/AutoShop/AutoTests.java
+++ src/test/java/com/AutoShop/AutoTests.java
@@ -1,29 +1,30 @@
 package com.AutoShop;
 
 import org.junit.Test;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.equalTo;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import java.net.HttpURLConnection;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.equalTo;
 
 public class AutoTests {
     private static final String ENDPOINT_URL = "http://localhost:8081";
-    private static final String DATABASE_URL = "jdbc:postgresql://localhost:5432/database?user=admin&password=admin";
 
     @Test
     public void connectWithDatabase() {
+        //given
+        final String DATABASE_URL = "jdbc:postgresql://localhost:5432/database?user=admin&password=admin";
+        //when
         try {
             Class.forName("org.postgresql.Driver");
             Connection connection = DriverManager.getConnection(DATABASE_URL);
-            System.out.println("Connection Established Successfull with DataBase: "
+            System.out.println("Connection Established Successful with DataBase: "
                     + connection.getMetaData().getDatabaseProductName());
         } catch (Exception e) {
             System.out.println("Unable to make connection with DB");
@@ -33,14 +34,26 @@ public class AutoTests {
 
     @Test
     public void pingTest() {
-        given().when().request("GET", "http://localhost:8081/allProducts").then().statusCode(200);
+        given()
+                .when()
+                .request("GET", UriComponentsBuilder
+                .fromHttpUrl(ENDPOINT_URL)
+                .path("/allProducts")
+                .build()
+                .toString())
+                .then()
+                .statusCode(200);
     }
 
     @Test
     public void verifyClientName() {
         given()
                 .when()
-                .get("http://localhost:8081/clients/1")
+                .get(UriComponentsBuilder
+                        .fromHttpUrl(ENDPOINT_URL)
+                        .path("/clients/1")
+                        .build()
+                        .toString())
                 .then()
                 .statusCode(200)
                 .body("lastName", equalTo("Grubinski"));
@@ -50,7 +63,10 @@ public class AutoTests {
     @Test
     public void shouldRemoveUser() throws IOException {
         // given
-        URL obj = new URL(UriComponentsBuilder.fromHttpUrl(ENDPOINT_URL).path("/deleteClient/2").build().toString());
+        URL obj = new URL(UriComponentsBuilder
+                .fromHttpUrl(ENDPOINT_URL)
+                .path("/deleteClient/2")
+                .build().toString());
         // when
         HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
         connection.setRequestMethod("DELETE");
@@ -58,6 +74,7 @@ public class AutoTests {
         assertThat(String.valueOf(connection.getResponseCode())).isEqualTo("200");
 
     }
+
     @Test
     public void shouldNotRemoveUser() throws IOException {
         // given
