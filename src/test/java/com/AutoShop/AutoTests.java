459c70009c92b8eafb3bde07fa10ce37183fb136
martyna <zarzyckamar@gmail.com>
2019-06-11 21:59:50 +0200

    connect with database

--- src/test/java/com/AutoShop/AutoTests.java
+++ src/test/java/com/AutoShop/AutoTests.java
@@ -1,4 +1,24 @@
 package com.AutoShop;
 
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class AutoTests {
+
+    @Test
+    public void connectWithDatabase() {
+        String Url = "jdbc:postgresql://localhost:5432/database?user=admin&password=admin";
+        try {
+            Class.forName("org.postgresql.Driver");
+            Connection connection = DriverManager.getConnection(Url);
+            System.out.println("Connection Established Successfull with DataBase: "
+                    + connection.getMetaData().getDatabaseProductName());
+        } catch (Exception e) {
+            System.out.println("Unable to make connection with DB");
+            e.printStackTrace();
+        }
+    }
+
 }
